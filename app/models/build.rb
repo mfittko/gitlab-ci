57cf8505ea8aa282c31c9839317700f8a4ccaa26
Manuel Fittko <mfittko@gmail.com>
2014-08-12 19:43:20 +0200

    updated reports folder structure

--- app/models/build.rb
+++ app/models/build.rb
@@ -146,7 +146,7 @@ class Build < ActiveRecord::Base
 
   def report_html(initial=false)
     html = ''
-    html_src = HTTParty.get("#{ENV['REPORTS_URL']}project-#{project_id}/rspec/#{sha}/#{id}")
+    html_src = HTTParty.get("#{ENV['REPORTS_URL']}project-#{project_id}/#{ref}/rspec/#{sha}/#{id}")
     parsed_html = Nokogiri::HTML(html_src)
     parsed_html.xpath("//script").remove
     if initial
