3a3a3b4376345ceeb718c0cf8afab7f59fb985a5
Manuel Fittko <mfittko@gmail.com>
2014-08-15 12:19:56 +0200

    …

--- app/models/build.rb
+++ app/models/build.rb
@@ -144,14 +144,16 @@ class Build < ActiveRecord::Base
     html ||= ''
   end
 
-  def report_html
-    html = ''
+  def report_html(full=false)
     html_src = HTTParty.get("#{ENV['REPORTS_URL']}project-#{project_id}/#{ref}/#{sha}/rspec/#{id}")
     html_src.gsub!("file:///home/gitlab_ci_runner/gitlab-ci-runner/tmp/reports/", ENV['REPORTS_URL']) if ENV['REPORTS_URL']
     parsed_html = Nokogiri::HTML(html_src)
     parsed_html.xpath("//script").remove
-    html += parsed_html.css('body > .rspec-report .results > .example_group').to_html
-    html
+    if full
+      parsed_html.to_html
+    else
+      parsed_html.css('body > .rspec-report .results > .example_group').to_html
+    end
   end
 
   def started?
