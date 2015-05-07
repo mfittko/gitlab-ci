7598808ff1f5b886b7d6319707ea3ef52f9b1e22
Manuel Fittko <manuel.fittko@columba.de>
2015-05-07 13:53:43 +0200

    …

--- app/models/build.rb
+++ app/models/build.rb
@@ -143,7 +143,7 @@ class Build < ActiveRecord::Base
     return @tags if !!@tags
     @tags = []
     begin
-      @tags = MultiJson.load(`curl --header "PRIVATE-TOKEN: #{GitlabCi.config.gitlab.private_token}" "http://col-git01.columba.intern/api/v3/projects/#{project_id}/repository/tags"`)
+      @tags = MultiJson.load(`curl --header "PRIVATE-TOKEN: #{GitlabCi.config.gitlab.private_token}" "http://col-git01.columba.intern/api/v3/projects/#{project.gitlab_id}/repository/tags"`)
       Rails.logger.info(@tags.first.to_s)
       @tags.select!{|t|t['commit']['id'] == sha}
     rescue Exception => e
